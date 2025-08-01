0625fc0b00fa5d10469cd2c89caf01801c5bdf07
Lars Schneidenbach <schneidenbach@us.ibm.com>
2025-08-01 17:06:32 -0400

    add event names for flex and torch
    Signed-off-by: Lars Schneidenbach <schneidenbach@us.ibm.com>

--- src/aiu_trace_analyzer/types.py
+++ src/aiu_trace_analyzer/types.py
@@ -17,6 +17,8 @@ class InputDialect:
         if category not in cls.categories:
             raise KeyError(f"ERROR: Category {category} is not part of this dialect.")
 
+        if entry == "-":
+            entry = None
         cls.dialect_map[category] = entry
         return True
 
@@ -31,7 +33,39 @@ class InputDialect:
 class InputDialectFLEX(InputDialect):
     _FLEX_DIALECT = {
         "NAME": "FLEX",
-        "AIU_Runtime": "AIU_Roundtrip",
+        "acc_launch_cb": "-",
+        "acc_graph_init": "-",
+        "acc_graph_exec": "Execute Graph",
+        "acc_malloc": "FixupAllocations",
+        "acc_resize_tensor_alloc": "AllocateFrame of graph",
+        "acc_supernode_launch": "Flex Roundtrip",
+        "acc_supernode_exec": "Flex Roundtrip",
+        "acc_node_compute": "Compute of $NodeName",
+        "acc_data_convert": "Compute of $NodeName-HostPrep",
+        "acc_scheduler_init": "SchedulerConstruct",
+        "acc_virtaddr_create": "CreatePipoIovas",
+        "acc_launch_schedule_compute": "ScheduleCompute",
+        "acc_schedule_wait": "WaitForCompletionAndReturnStatus",
+        "acc_dma_prep": "PrepareDmas",
+        "acc_rdma_prep_sync": "PrepareAndSyncRdma",
+        "acc_cache_clear": "LaunchClearScratchpad",
+        "acc_cache_preload": "LaunchPreloadScratchpad",
+        "acc_launch_compute_stream": "LaunchComputeStream",
+        "acc_rdma_barrier1": "Barrier1",
+        "acc_rdma_post_keys": "PostKeys",
+        "acc_rdma_barrier2": "Barrier2",
+        "acc_rdma_fetch_keys": "FetchKeys",
+        "acc_rdma_update_cb": "Update CBs",
+        "acc_rdma_barrier3": "Barrier3",
+        "acc_rdma_check_deadlock": "Deadlock Check",
+        "acc_filetransfer_DtoF": "-",
+        "acc_filetransfer_MtoF": "-",
+        "acc_filetransfer_FtoD": "-",
+        "acc_filetransfer_FtoM": "-",
+        "acc_datatransfer_DtoH": "-",
+        "acc_datatransfer_HtoD": "-",
+        "acc_clock_calibration": "-",
+        "acc_compile_graph": "-",
     }
 
     def __new__(cls):
@@ -45,7 +79,39 @@ class InputDialectFLEX(InputDialect):
 class InputDialectTORCH(InputDialect):
     _TORCH_DIALECT = {
         "NAME": "TORCH",
-        "AIU_Runtime": "AIU_Runtime",
+        "acc_launch_cb": "aiuLaunchControlBlocks",
+        "acc_graph_init": "aiuInitGraph",
+        "acc_graph_exec": "aiuGraphExecution",
+        "acc_malloc": "aiuMalloc",
+        "acc_resize_tensor_alloc": "aiuResizeTensorAllocation",
+        "acc_supernode_launch": "aiuLaunchSuperNode",
+        "acc_supernode_exec": "aiuSuperNodeExecution",
+        "acc_node_compute": "aiuNodeCompute",
+        "acc_data_convert": "aiuDataConvert",
+        "acc_scheduler_init": "aiuInitScheduler",
+        "acc_virtaddr_create": "aiuCreateVirtualAddresses",
+        "acc_launch_schedule_compute": "aiuLaunchScheduleCompute",
+        "acc_schedule_wait": "aiuScheduleWait",
+        "acc_dma_prep": "aiuPrepareDMAs",
+        "acc_rdma_prep_sync": "aiuPrepareAndSyncRDMA",
+        "acc_cache_clear": "aiuClearCache",
+        "acc_cache_preload": "aiuPreloadCache",
+        "acc_launch_compute_stream": "aiuLaunchComputeStream",
+        "acc_rdma_barrier1": "aiuRDMABarrier1",
+        "acc_rdma_post_keys": "aiuPostRDMAKeys",
+        "acc_rdma_barrier2": "aiuRDMABarrier2",
+        "acc_rdma_fetch_keys": "aiuFetchRDMAKeys",
+        "acc_rdma_update_cb": "aiuUpdateRDMACBs",
+        "acc_rdma_barrier3": "aiuRDMABarrier3",
+        "acc_rdma_check_deadlock": "aiuCheckRDMADeadlock",
+        "acc_filetransfer_DtoF": "aiuFileTransferDtoF",
+        "acc_filetransfer_MtoF": "aiuFileTransferMtoF",
+        "acc_filetransfer_FtoD": "aiuFileTransferFtoD",
+        "acc_filetransfer_FtoM": "aiuFileTransferFtoM",
+        "acc_datatransfer_DtoH": "aiuDataTransferDtoH",
+        "acc_datatransfer_HtoD": "aiuDataTransferHtoD",
+        "acc_clock_calibration": "aiuClockCalibration",
+        "acc_compile_graph": "aiuCompileGraph",
     }
 
     def __new__(cls):
